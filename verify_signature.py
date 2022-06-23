cf3e961d52d11fbe4450860b32de1478b497c995
Rosalie Tolentino <rosalie.tolentino@checkr.com>
2022-06-23 14:31:07 -0700

    Use autopep8 style guide for python file

--- verify_signature.py
+++ verify_signature.py
@@ -1,18 +1,22 @@
 import hmac
 import hashlib
-import json 
+import json
 
-def verifySignature(secret, payload, signature):
-  computedHash = hmac.new(bytes(secret, 'UTF-8'), bytes(payload, 'UTF-8'), hashlib.sha256).hexdigest()
-  return hmac.compare_digest(signature, computedHash)
+
+def verify_signature(secret, payload, signature):
+    computed_hash = hmac.new(bytes(secret, 'UTF-8'),
+                             bytes(payload, 'UTF-8'), hashlib.sha256).hexdigest()
+    return hmac.compare_digest(signature, computed_hash)
+
 
 payload = {
-  'monday': '75F',
-  'tuesday': '80F'
+    'monday': '75F',
+    'tuesday': '80F'
 }
 
-payloadSignature = 'b7412f05e981a473b5ecbdb5393afaea02a679db6d7c8e56803512ec4ba98151'
+payload_signature = 'b7412f05e981a473b5ecbdb5393afaea02a679db6d7c8e56803512ec4ba98151'
 
 secret = 'test-secret'
-payloadJSON = json.dumps(payload, separators=(',', ':'))
-print(verifySignature(secret, payloadJSON, payloadSignature)) # should print true
+payload_json = json.dumps(payload, separators=(',', ':'))
+print(verify_signature(secret, payload_json,
+      payload_signature))  # should print true
