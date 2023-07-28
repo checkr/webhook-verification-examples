b50fd3b205b5cd7628a7e514604973c37e1680a4
Franco Soto <franco.soto@checkr.com>
2023-07-28 12:17:19 -0400

    Fix json object ingest for python usage

--- verify_signature.py
+++ verify_signature.py
@@ -9,11 +9,16 @@ def verify_signature(secret, compact_json_payload, signature):
     return hmac.compare_digest(signature, computed_hash)
 
 
-payload = {
-    'monday': '75F',
-    'tuesday': '80F'
-}
+json_string = '''{
+    "monday": "75F",
+    "tuesday": "80F"
+}'''
+
 # Warning! JSON must be compact. Pretty JSON with spaces will not work.
+# First convert the json string to a Python object
+payload = json.loads(json_string)
+
+# Then from a Python object to a compact json string
 compact_json_payload = json.dumps(payload, separators=(',', ':'))
 
 payload_signature = 'b7412f05e981a473b5ecbdb5393afaea02a679db6d7c8e56803512ec4ba98151'
